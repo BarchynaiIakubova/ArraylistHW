da32a670c2c89330cfb0af346244d49df6cf57d1
barchynaiiakubova <barchynai.iakubova@gmail.com>
2022-03-20 14:17:45 +0600

    ArraylistHW

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,32 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("heelo Barchynai");
-
+        Random random = new Random();
+        ArrayList<Integer> arraylist = new ArrayList<>();
+        ArrayList<Integer> evenNumber = new ArrayList<>();
+        ArrayList<Integer> oddNumber = new ArrayList<>();
+        for (int i = 0;i < 50;i++) {
+            arraylist.add(random.nextInt(100) + 1);
+        }
+        for (int i:arraylist) {
+            if (i % 2 == 0) {
+                evenNumber.add(i);
+            }
+            else oddNumber.add(i);
+        }
+        System.out.println("Even numbers are: ");
+        print(evenNumber);
+        System.out.println("\nOdd numbers are: ");
+        print(oddNumber);
+    }
+    public static void print(ArrayList<Integer> array) {
+        for (int i: array) {
+            System.out.print(i + " ");
+        }
     }
 }
